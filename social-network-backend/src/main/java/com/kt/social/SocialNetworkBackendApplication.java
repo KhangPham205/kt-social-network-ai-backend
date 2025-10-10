1b07e1c5d1173a799743f023d8c1574a508e0a71
KhangPham205 <ptkhang17122005@gmail.com>
2025-10-10 16:08:58 +0700

    init backend infra

---
+++ social-network-backend/src/main/java/com/kt/social/SocialNetworkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kt.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkBackendApplication.class, args);
+	}
+
+}
