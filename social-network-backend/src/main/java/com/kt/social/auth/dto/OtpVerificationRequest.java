5208b20ffce126d9afeceaa6ba37823351829f78
KhangPham205 <ptkhang17122005@gmail.com>
2025-10-20 22:51:07 +0700

    Update OtpVerificationRequest.java

--- social-network-backend/src/main/java/com/kt/social/auth/dto/OtpVerificationRequest.java
+++ social-network-backend/src/main/java/com/kt/social/auth/dto/OtpVerificationRequest.java
@@ -1,23 +1,17 @@
 package com.kt.social.auth.dto;
 
-<<<<<<< Updated upstream:social-network-backend/src/main/java/com/kt/social/auth/dto/VerifyResetCodeRequest.java
-import lombok.Data;
-
-@Data
-public class VerifyResetCodeRequest {
-=======
 import com.kt.social.auth.enums.OtpType;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class OtpVerificationRequest {
     @NotBlank
     @Email
->>>>>>> Stashed changes:social-network-backend/src/main/java/com/kt/social/auth/dto/OtpVerificationRequest.java
     private String email;
     private String code;
 
